3177389df48f06f0efd2f66e308c0602ab2bd194
Asantini <abe.santini@gmail.com>
2014-11-22 13:24:12 -0800

    finished initial problem_003 attempt for java.

--- java/src/problem_003.java
+++ java/src/problem_003.java
@@ -2,47 +2,66 @@
 * The prime factors of 13195 are 5, 7, 13 and 29.
 *
 * What is the largest prime factor of the number 600851475143 ?
+* https://projecteuler.net/problem=3
 */
+import java.util.*;
 
 public class problem_003 {
-	private  List<Integer> knownPrimes;
 	
 	public static void main(String[] args) {
-		knownPrimes = new ArrayList<Integer>();
-		knownPrimes.add(new Integer(2));
-		int number = getInteger(args[0]);
+		KnownPrimes kp = new KnownPrimes();
+		long number = Long.parseLong(args[0]);
 		int index = 0;
-		int test = knownPrimes.get(index).valueOf();
-		boolean continue = true;
-		while (number != 0) {
-			while(continue) {
-				if(number % test == 0) {
-					number /= test;
-				} else {
-					test = getInteger
-				}
+		long test = kp.getNext();
+		while (number != 1) {
+			if(number % test == 0) {
+				number /= test;
+			} else {
+				test = kp.getNext();
 			}
 		}
-	
+		System.out.println(test);
 	
 	} // end main
 	
-	private static Integer nextPrime() {
-		int test = knownPrimes.get(knownPrimes.size() -1).valueOf() + 1;
-		boolean continue = true;
-		while(continue){
-			ListIterator<Integer> iter = knownPrimes.listIterator(0);
-			while(iter.hasNext()){
-				int divisor = iter.next().valueOf();
-				if(test % divisor == 0) {
-					test++;
-					break;
-				}
+
+
+	private static class KnownPrimes {
+	
+		private List<Long> knownPrimes; 
+		private int index; 
+		
+		private KnownPrimes() {
+			knownPrimes = new ArrayList<Long>();
+			knownPrimes.add(new Long(2));
+			index = 0;
+		} // end KnownPrimes const
+		
+		public long getNext() {
+			if(index == knownPrimes.size()) {
+				knownPrimes.add(nextPrime());
 			}
-			continue = false;
+			return knownPrimes.get(index++).longValue();
+		} // end getNext
+		
+		private Long nextPrime() {
+			long test = knownPrimes.get(knownPrimes.size() -1).longValue() + 1;
+			boolean cont = true;
+			while(cont){
+				ListIterator<Long> iter = knownPrimes.listIterator(0);
+				while(iter.hasNext()){
+					long denom = iter.next().longValue();
+					if(test % denom == 0) {
+						test++;
+						break;
+					}
+				}
+				cont = false;
+			}
+			return new Long(test);
 		}
-		return new Integer(test);
-	}
+		
+	}// end KnownPrimes
 
 
 
