83efc0acae6ace61f0eb3cc0d8e787314d65e8c1
Asantini <abe.santini@gmail.com>
2014-12-16 21:56:43 -0800

    Initial commit of python version of prob 3

--- python/problem_003.py
+++ python/problem_003.py
@@ -7,16 +7,76 @@
 
 import sys
 import os.path
-import json
-imprt io
+import math
+import io
 
-n = int(sys.argv[1]
-
-kp = none
-
-if(os.path.isfile('kp.json')):
-	json_data = open('kp.json')
-	kp = json.load(json_data)
-else:
-	kp = [2,3]
+def resetFile(filename):
+	os.remove(filename)
+	inFile = open(filename, "a+")
+	inFile.write('2\n')
+	inFile.write('3\n')
+	return inFile
 	
+def getNextPrime(index, kpFile):
+	returnNum = None
+	try:
+		kpFile.seek(index)
+		returnNum = int(kpFile.readline())
+	except:
+		kpFile.seek(index - 1)
+		test = int(kpFile.readline()) + 2
+		testSqrt = math.sqrt(test)
+		testIndex = 1
+		while(True):
+			kpFile.seek(testIndex)
+			testIndex += 1
+			demon = int(kpFile.readline())
+			print demon
+			if denom > testSqrt:
+				kpFile.seek(index)
+				kpFile.write(str(test) + '\n')
+				returnNum = test
+				break
+			elif test % demon == 0:
+				test += 2
+				testSqrt = math.sqrt(test)
+				testIndex = 1
+			
+	return returnNum
+	
+
+n = int(sys.argv[1])
+# open file and test to make sure it is valid.
+filename = 'kp.txt'
+kp = open(filename, "a+")
+lines = 0
+for line in kp:
+	lines += 1
+	if lines == 2:
+		break
+			
+if lines < 2:
+	kp = resetFile(filename)
+else:
+	kp.seek(0)
+	test1 = int(kp.readline())
+	test2 = int(kp.readline())
+	if(test1 != 2 or test2 != 3):
+		kp = resetFile(filename)
+	
+index = 0
+kp.seek(index)
+test_num = int(kp.readline())
+
+while(n != 1):
+	if(n % test_num == 0):
+		n /= test_num
+	else:
+		index += 1
+		test_num = getNextPrime(index,kp)
+			
+	
+print(test_num)
+
+
+
